acaa40f969624d9f50a4ed8f1676be18ca61a60a
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-11 08:59:38 +0000

    Add real working example

--- example.js
+++ example.js
@@ -2,12 +2,14 @@ var bs = require('browser-sync').create();
 var mod = require('path').dirname(require.resolve('./'));
 
 bs.init({
-    proxy: 'http://www.bbc.co.uk',
-    plugins: [mod],
+    proxy:        'http://www.bbc.co.uk',
+    plugins:      [mod],
+    serveStatic:  ['tests'],
+    files:        ['tests'],
     rewriteRules: [
         {
-            match: 'shane',
-            replace: 'kittie'
+            match: 'http://static.bbci.co.uk/frameworks/barlesque/2.93.13/orb/4/style/orb.min.css',
+            replace: '/fixtures/bbc.css'
         }
     ]
 });
